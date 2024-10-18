1729605ad55928e02388ac14d3e569d84726eced
Dimitrii-Kozlov <demetra_n@front.ru>
2024-10-18 23:02:11 +0300

    add base/crud

--- presentations/crud.py
+++ presentations/crud.py
@@ -1,19 +1,11 @@
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.future import select
 from database import async_session_maker
 from presentations.models import Presentation
+from base.crud import BaseCRUD
 
 
-class PresentationCRUD:
+class PresentationCRUD(BaseCRUD):
     model = Presentation
-
-    @classmethod
-    async def find_one_or_none_by_id(cls, id: int):
-        async with async_session_maker() as session:
-            query = select(cls.model).filter_by(id=id)
-            result = await session.execute(query)
-            plain_result = result.scalar_one_or_none()
-            return plain_result
 
     @classmethod
     async def find_presentation_by_filter(cls, **filter):
@@ -26,24 +18,3 @@ class PresentationCRUD:
             result = await session.execute(query)
             plain_result = result.scalars().all()
             return plain_result
-
-    @classmethod
-    async def find_all(cls):
-        async with async_session_maker() as session:
-            query = select(cls.model)
-            result = await session.execute(query)
-            plain_result = result.scalars().all()
-            return plain_result
-
-    @classmethod
-    async def add(cls, **values):
-        async with async_session_maker() as session:
-            async with session.begin():
-                new_instance = cls.model(**values)
-                session.add(new_instance)
-                try:
-                    await session.commit()
-                except SQLAlchemyError as e:
-                    await session.rollback()
-                    raise e
-                return new_instance
